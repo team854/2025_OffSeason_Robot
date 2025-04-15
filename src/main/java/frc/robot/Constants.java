12479954937fbda13dedffa5b56426633ed3b766
Fireblade <fireblade5234@gmail.com>
2025-04-14 21:22:58 -0400

    Port over the elevator subsystem (Still needs testing)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -33,35 +33,35 @@ public final class Constants {
 		public static final Distance X = Feet.of(24.95316667);
 		public static final Distance Y = Meter.of(APRIL_TAG_FIELD_LAYOUT.getFieldWidth() / 2);
 	}
-	
+
 	public static final boolean USE_WELDED_FIELD = false;
 	public static final AprilTagFieldLayout APRIL_TAG_FIELD_LAYOUT = AprilTagFieldLayout.loadField(USE_WELDED_FIELD ? AprilTagFields.k2025ReefscapeWelded : AprilTagFields.k2025ReefscapeAndyMark);
 
 	public static class SwerveConstants {
-	
+
 		public static class SwerveModuleConstants {
 			public static class BackLeft {
 				public static class Location {
 					public static final Distance FRONT = Inch.of(-14.75); // Inches
 					public static final Distance LEFT = Inch.of(9.875); // Inches
 				}
-		
+
 				public static final Angle ABSOLUTE_ENCODER_OFFSET = Degree.of(308.8381620); // Degrees
-		
+
 				public static class DriveMotor {
 					public static final String TYPE = "sparkmax_neo";
 					public static final int ID = 23;
 					public static final Object CANBUS = null;
 					public static final boolean INVERTED = false;
 				}
-		
+
 				public static class AngleMotor {
 					public static final String TYPE = "sparkmax_neo";
 					public static final int ID = 33;
 					public static final Object CANBUS = null;
 					public static final boolean INVERTED = true;
 				}
-		
+
 				public static class Encoder {
 					public static final String TYPE = "thrifty";
 					public static final int ID = 3;
@@ -69,29 +69,29 @@ public final class Constants {
 					public static final boolean INVERTED = false;
 				}
 			}
-		
+
 			public static class BackRight {
 				public static class Location {
 					public static final Distance FRONT = Inch.of(-14.75); // Inches
 					public static final Distance LEFT = Inch.of(-9.875); // Inches
 				}
-		
+
 				public static final Angle ABSOLUTE_ENCODER_OFFSET = Degree.of(150.9974208105); // Degrees
-		
+
 				public static class DriveMotor {
 					public static final String TYPE = "sparkmax_neo";
 					public static final int ID = 22;
 					public static final Object CANBUS = null;
 					public static final boolean INVERTED = false;
 				}
-		
+
 				public static class AngleMotor {
 					public static final String TYPE = "sparkmax_neo";
 					public static final int ID = 32;
 					public static final Object CANBUS = null;
 					public static final boolean INVERTED = true;
 				}
-		
+
 				public static class Encoder {
 					public static final String TYPE = "thrifty";
 					public static final int ID = 2;
@@ -99,29 +99,29 @@ public final class Constants {
 					public static final boolean INVERTED = false;
 				}
 			}
-		
+
 			public static class FrontLeft {
 				public static class Location {
 					public static final Distance FRONT = Inch.of(14.75); // Inches
 					public static final Distance LEFT = Inch.of(9.875); // Inches
 				}
-		
+
 				public static final Angle ABSOLUTE_ENCODER_OFFSET = Degree.of(19.47493551); // Degrees
-		
+
 				public static class DriveMotor {
 					public static final String TYPE = "sparkmax_neo";
 					public static final int ID = 20;
 					public static final Object CANBUS = null;
 					public static final boolean INVERTED = false;
 				}
-		
+
 				public static class AngleMotor {
 					public static final String TYPE = "sparkmax_neo";
 					public static final int ID = 30;
 					public static final Object CANBUS = null;
 					public static final boolean INVERTED = true;
 				}
-		
+
 				public static class Encoder {
 					public static final String TYPE = "thrifty";
 					public static final int ID = 0;
@@ -129,29 +129,29 @@ public final class Constants {
 					public static final boolean INVERTED = false;
 				}
 			}
-		
+
 			public static class FrontRight {
 				public static class Location {
 					public static final Distance FRONT = Inch.of(14.75); // Inches
 					public static final Distance LEFT = Inch.of(-9.875); // Inches
 				}
-		
+
 				public static final Angle ABSOLUTE_ENCODER_OFFSET = Degree.of(112.15935540); // Degrees
-		
+
 				public static class DriveMotor {
 					public static final String TYPE = "sparkmax_neo";
 					public static final int ID = 21;
 					public static final Object CANBUS = null;
 					public static final boolean INVERTED = false;
 				}
-		
+
 				public static class AngleMotor {
 					public static final String TYPE = "sparkmax_neo";
 					public static final int ID = 31;
 					public static final Object CANBUS = null;
 					public static final boolean INVERTED = true;
 				}
-		
+
 				public static class Encoder {
 					public static final String TYPE = "thrifty";
 					public static final int ID = 1;
@@ -160,7 +160,7 @@ public final class Constants {
 				}
 			}
 		}
-	
+
 		public static class Imu {
 			public static final String TYPE = "navx";
 			public static final int ID = 0;
@@ -169,14 +169,14 @@ public final class Constants {
 			public static final Angle OFFSET = Degree.of(0.0); // Degrees
 			public static final double ANGULAR_VELOCITY_COEFF = 0.1;
 		}
-	
+
 		public static class MotorConstants {
 			public static class Angle {
 				public static final Time RAMP_RATE = Second.of(0); // Seconds
 				public static final double GEAR_RATIO = 21.4285714286;
 				public static final double FACTOR = 0;
 				public static final Current CURRENT_LIMIT = Amp.of(20.0); // Amps
-		
+
 				public static class Pidf {
 					public static final double P = 0.055;
 					public static final double I = 0;
@@ -185,13 +185,13 @@ public final class Constants {
 					public static final double IZ = 0;
 				}
 			}
-		
+
 			public static class Drive {
 				public static final Time RAMP_RATE = Second.of(0); // Seconds
 				public static final double GEAR_RATIO = 6.75;
 				public static final double FACTOR = 0;
 				public static final Current CURRENT_LIMIT = Amp.of(40.0); // Amps
-		
+
 				public static class Pidf {
 					public static final double P = 0.006;
 					public static final double I = 0;
@@ -200,7 +200,7 @@ public final class Constants {
 					public static final double IZ = 0;
 				}
 			}
-	
+
 			public static final Voltage OPTIMAL_VOLTAGE = Volt.of(12.0); // Volts
 		}
 
@@ -209,7 +209,7 @@ public final class Constants {
 			public static final double I = 0;
 			public static final double D = 0.1;
 		}
-	
+
 		public static class HeadingPID {
 			public static final double P = 7;
 			public static final double I = 0;
@@ -220,7 +220,7 @@ public final class Constants {
 			public static final double WHEEL_GRIP_COF = 1.19;
 			public static final Distance DIAMETER = Inch.of(4.0); // Inches
 		}
-		
+
 		public static final double ANGLE_JOYSTICK_RADIUS_DEADBAND = 0.5;
 		public static final List<String> MODULE_FILES = List.of("frontleft.json", "frontright.json", "backleft.json", "backright.json");
 		public static final String SWERVECONFIGDIR = "swerve";
@@ -238,7 +238,7 @@ public final class Constants {
 		public static final AngularAcceleration MAX_ANGULAR_ACCELERATION = DegreesPerSecondPerSecond.of(720); // Degrees/Second/Second
 		public static final double MOI = 6.883;
 		public static final String DRIVE_MOTOR_TYPE = "NEO";
-	
+
 		public static class BumperOffset {
 			public static final Distance X = Feet.of(0); // Feet
 			public static final Distance Y = Feet.of(0); // Feet
@@ -247,44 +247,101 @@ public final class Constants {
 
 	public static class ElevatorConstants {
 		public static class Stage1 {
-		  public static final int ID = 3;
-		  public static final double MAX_VELOCITY = 120; // Not sure the unit
-		  public static final double MAX_ACCELERATION = 200; // Not sure the unit
-		  public static final double P = 67;
-		  public static final double I = 0;
-		  public static final double D = 1.15;
-		  public static final Voltage S = Volt.of(0.05); // Volts
-		  public static final Voltage G = Volt.of(1.36); // Volts
-		  public static final Voltage V = Volt.of(17.0); // Volts/(Meters/Second)
-		  public static final Voltage A = Volt.of(0.2); // Volts/(Meters/Second^2) 
-		  public static final Mass MASS = Pound.of(50); // Pounds
-		  public static final Distance DRUM_RADIUS = Inch.of(0.98110236); // Inches
-		  public static final double GEAR_RATIO = 18.5714;
-		  public static final Distance HARD_MAX_HEIGHT = Feet.of(2.25); // Feet
-		  public static final Distance TOLLERANCE = Feet.of(0.2); // Feet
+			public static final int ID = 3;
+			public static final double MAX_VELOCITY = 120; // Not sure the unit
+			public static final double MAX_ACCELERATION = 200; // Not sure the unit
+			public static final double P = 67;
+			public static final double I = 0;
+			public static final double D = 1.15;
+			public static final Voltage S = Volt.of(0.05); // Volts
+			public static final Voltage G = Volt.of(1.36); // Volts
+			public static final Voltage V = Volt.of(17.0); // Volts/(Meters/Second)
+			public static final Voltage A = Volt.of(0.2); // Volts/(Meters/Second^2)
+			public static final Mass MASS = Pound.of(50); // Pounds
+			public static final Distance DRUM_RADIUS = Inch.of(0.98110236); // Inches
+			public static final double GEAR_RATIO = 18.5714;
+			public static final Distance HARD_MAX_HEIGHT = Feet.of(2.25); // Feet
+			public static final Distance TOLLERANCE = Feet.of(0.2); // Feet
 		}
-	
+
 		public static class Stage2 {
-		  public static final int ID = 2;
-		  public static final double MAX_VELOCITY = 120; // Not sure the unit
-		  public static final double MAX_ACCELERATION = 200; // Not sure the unit
-		  public static final double ABSOLUTE_ENCODER_OFFSET = 0; // Degrees
-		  public static final double P = 66;
-		  public static final double I = 0;
-		  public static final double D = 1.15;
-		  public static final Voltage S = Volt.of(0.05); // Volts
-		  public static final Voltage G = Volt.of(0.65); // Volts
-		  public static final Voltage V = Volt.of(14.52); // Volts/(Meters/Second)
-		  public static final Voltage A = Volt.of(0.04); // Volts/(Meters/Second^2) 
-		  public static final Mass MASS = Pound.of(35); // Pounds
-		  public static final Distance DRUM_RADIUS = Inch.of(0.98110236); // Inches
-		  public static final double GEAR_RATIO = 15.7143;
-		  public static final Distance HARD_MAX_HEIGHT = Feet.of(2.12); // Feet
-		  public static final Distance TOLLERANCE = Feet.of(0.2); // Feet
+			public static final int ID = 2;
+			public static final double MAX_VELOCITY = 120; // Not sure the unit
+			public static final double MAX_ACCELERATION = 200; // Not sure the unit
+			public static final double P = 66;
+			public static final double I = 0;
+			public static final double D = 1.15;
+			public static final Voltage S = Volt.of(0.05); // Volts
+			public static final Voltage G = Volt.of(0.65); // Volts
+			public static final Voltage V = Volt.of(14.52); // Volts/(Meters/Second)
+			public static final Voltage A = Volt.of(0.04); // Volts/(Meters/Second^2)
+			public static final Mass MASS = Pound.of(35); // Pounds
+			public static final Distance DRUM_RADIUS = Inch.of(0.98110236); // Inches
+			public static final double GEAR_RATIO = 15.7143;
+			public static final Distance HARD_MAX_HEIGHT = Feet.of(2.12); // Feet
+			public static final Distance TOLLERANCE = Feet.of(0.2); // Feet
 		}
-	
-		public static final double ZERO_HEIGHTS_ABOVE_BASE = 0.520; // Feet
-	  }
+
+		public static final Distance ZERO_HEIGHTS_ABOVE_BASE = Feet.of(0.520); // Feet
+	}
+
+	public static class ArmConstants {
+		public static class Shoulder {
+			public static final int ID = 4;
+			public static final double MAX_VELOCITY = 120; // Not sure the unit
+			public static final double MAX_ACCELERATION = 200; // Not sure the unit
+			public static final double P = 0.6;
+			public static final double I = 0;
+			public static final double D = 0.08;
+			public static final Voltage S = Volt.of(0.05); // Volts
+			public static final Voltage G = Volt.of(3.45); // Volts
+			public static final Voltage V = Volt.of(0.01); // Volts/(Degrees/Second)
+			public static final Voltage A = Volt.of(0.02); // Volts/(Degrees/Second^2)
+			public static final Mass MASS = Pound.of(10.0); // Pounds
+			public static final Angle MIN_ANGLE = Degree.of(-80.0); // Degrees
+			public static final Angle MAX_ANGLE = Degree.of(80.0); // Degrees
+			public static final Angle ABSOLUTE_ENCODER_OFFSET = Degree.of(0); // Degrees
+			public static final double GEAR_RATIO = 50;
+			public static final double ABSOLUTE_ENCODER_GEAR_RATIO = 1.85714;
+			public static final Angle ABSOLUTE_ENCODER_PUSH_BACK = Degree.of(25.0); // Degrees
+			public static final Distance CENTER_OFFSET_FOWARD = Feet.of(0.492126); // Feet
+			public static final Distance STAGE_OFFSET_UP = Feet.of(0.958); // Feet
+			public static final Angle TOLLERANCE = Degree.of(3.0); // Degrees
+		}
+
+		public static class Wrist {
+			public static final int ID = 5;
+			public static final double MAX_VELOCITY = 120; // Not sure the unit
+			public static final double MAX_ACCELERATION = 200; // Not sure the unit
+			public static final double P = 0.5;
+			public static final double I = 0;
+			public static final double D = 0.05;
+			public static final Voltage S = Volt.of(0.05); // Volts
+			public static final Voltage G = Volt.of(3.48); // Volts
+			public static final Voltage V = Volt.of(0.01); // Volts/(Degrees/Second)
+			public static final Voltage A = Volt.of(0.02); // Volts/(Degrees/Second^2)
+			public static final Mass MASS = Pound.of(15.0); // Pounds
+			public static final double MOI = 0.05; // Moment of innertia jKgMetersSquared
+			public static final double GEAR_RATIO = 400;
+			public static final Angle TOLLERANCE = Degree.of(2); // Degrees
+		}
+
+		public static class Intake {
+			public static final int ID = 6;
+
+			public static class Simulation {
+				public static final Distance WIDTH = Feet.of(0.7); // Feet
+				public static final Distance LENGTH = Feet.of(0.6); // Feet
+			}
+		}
+
+		public static class IntakeSensor {
+			public static final int ID = 0;
+		}
+
+		public static final Distance LENGTH = Feet.of(2.081208); // Feet
+
+	}
 
 	public static class DriverConstants {
 		public static final int PORT = 0;
--- src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/elevator/ElevatorSubsystem.java
@@ -1,17 +1,37 @@
 package frc.robot.subsystems.elevator;
 
+import static edu.wpi.first.units.Units.Degree;
+import static edu.wpi.first.units.Units.Kilogram;
 import static edu.wpi.first.units.Units.Meter;
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import static edu.wpi.first.units.Units.Volt;
 
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.sim.SparkMaxSim;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ElevatorFeedforward;
+import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.wpilibj.simulation.BatterySim;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj.simulation.RoboRioSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.Robot;
+import frc.robot.RobotContainer;
+
+// Credit to https://www.youtube.com/watch?v=_2fPVYDrq_E for the great tutorial
 
 public class ElevatorSubsystem extends SubsystemBase {
 	/*
@@ -27,14 +47,21 @@ public class ElevatorSubsystem extends SubsystemBase {
 	private final SparkMax stage1Motor = new SparkMax(Constants.ElevatorConstants.Stage1.ID, MotorType.kBrushless);
 	private final RelativeEncoder stage1Encoder = stage1Motor.getEncoder();
 
-    private final SparkMax stage2Motor = new SparkMax(Constants.ElevatorConstants.Stage2.ID, MotorType.kBrushless);
-    private final RelativeEncoder stage2Encoder = stage2Motor.getEncoder();
-    private final AbsoluteEncoder stage2AbsoluteEncoder = stage2Motor.getAbsoluteEncoder();
+	private final SparkMax stage2Motor = new SparkMax(Constants.ElevatorConstants.Stage2.ID, MotorType.kBrushless);
+	private final RelativeEncoder stage2Encoder = stage2Motor.getEncoder();
+
+	/*
+	 * Control
+	 */
+	private final ProfiledPIDController stage1Controller;
+	private final ElevatorFeedforward stage1FeedFoward;
+
+	private final ProfiledPIDController stage2Controller;
+	private final ElevatorFeedforward stage2FeedFoward;
 
 	/*
 	 * Simulation
 	 */
-
 	private final DCMotor stage1Gearbox = DCMotor.getNEO(1);
 	private final SparkMaxSim stage1MotorSim = new SparkMaxSim(stage1Motor, stage1Gearbox);
 	private ElevatorSim stage1ElevatorSim = null;
@@ -42,4 +69,245 @@ public class ElevatorSubsystem extends SubsystemBase {
 	private final DCMotor stage2Gearbox = DCMotor.getNEO(1);
 	private final SparkMaxSim stage2MotorSim = new SparkMaxSim(stage2Motor, stage2Gearbox);
 	private ElevatorSim stage2ElevatorSim = null;
+
+	public ElevatorSubsystem() {
+
+		/*
+		 * Configure motors
+		 */
+
+		System.out.println("Configuring elevator motors");
+
+		// Configure stage 1 and save it to the motor
+		SparkMaxConfig stage1Config = new SparkMaxConfig();
+		stage1Config.idleMode(IdleMode.kBrake); // Brake so the stage doesn't fall
+		stage1Config.inverted(false);
+
+		// Configure the encoder so they automaticlly convert the motors rotation to meters
+		double stage1Conversion = (Constants.ElevatorConstants.Stage1.DRUM_RADIUS.in(Meter) * 2 * Math.PI) / Constants.ElevatorConstants.Stage1.GEAR_RATIO;
+		stage1Config.encoder.positionConversionFactor(stage1Conversion);
+		stage1Config.encoder.velocityConversionFactor(stage1Conversion);
+		stage1Motor.configure(stage1Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+		// Configure stage 2 and save it to the motor
+		SparkMaxConfig stage2Config = new SparkMaxConfig();
+		stage2Config.idleMode(IdleMode.kBrake); // Brake so the stage doesn't fall
+		stage2Config.inverted(false);
+		stage2Config.absoluteEncoder.inverted(false);
+
+		// Configure the encoder so they automaticlly convert the motors rotation to meters
+		double stage2Conversion = (Constants.ElevatorConstants.Stage2.DRUM_RADIUS.in(Meter) * 2 * Math.PI) / Constants.ElevatorConstants.Stage2.GEAR_RATIO;
+		stage2Config.encoder.positionConversionFactor(stage2Conversion);
+		stage2Config.encoder.velocityConversionFactor(stage2Conversion);
+		stage2Motor.configure(stage2Config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+		/*
+		 * Configure PIDS
+		 */
+
+		// Define the trapizoid profile for the pids
+		TrapezoidProfile.Constraints stage1Constraints = new TrapezoidProfile.Constraints(
+				Constants.ElevatorConstants.Stage1.MAX_VELOCITY, Constants.ElevatorConstants.Stage1.MAX_ACCELERATION);
+		TrapezoidProfile.Constraints stage2Constraints = new TrapezoidProfile.Constraints(
+				Constants.ElevatorConstants.Stage2.MAX_VELOCITY, Constants.ElevatorConstants.Stage2.MAX_ACCELERATION);
+
+		// Initalize the motor pids
+		stage1Controller = new ProfiledPIDController(Constants.ElevatorConstants.Stage1.P,
+				Constants.ElevatorConstants.Stage1.I, Constants.ElevatorConstants.Stage1.D, stage1Constraints);
+		stage2Controller = new ProfiledPIDController(Constants.ElevatorConstants.Stage2.P,
+				Constants.ElevatorConstants.Stage2.I, Constants.ElevatorConstants.Stage2.D, stage2Constraints);
+
+		// Configure feed foward
+		stage1FeedFoward = new ElevatorFeedforward(
+				Constants.ElevatorConstants.Stage1.S.in(Volt),
+				Constants.ElevatorConstants.Stage1.G.in(Volt),
+				Constants.ElevatorConstants.Stage1.V.in(Volt),
+				Constants.ElevatorConstants.Stage1.A.in(Volt));
+		stage2FeedFoward = new ElevatorFeedforward(
+				Constants.ElevatorConstants.Stage2.S.in(Volt),
+				Constants.ElevatorConstants.Stage2.G.in(Volt),
+				Constants.ElevatorConstants.Stage2.V.in(Volt),
+				Constants.ElevatorConstants.Stage2.A.in(Volt));
+
+		/*
+         * Initalize simulation
+         */
+        if (Robot.isSimulation()) {
+            System.out.println("Creating elevator simulations");
+            stage1ElevatorSim = new ElevatorSim(
+					stage1Gearbox,
+					Constants.ElevatorConstants.Stage1.GEAR_RATIO,
+                    Constants.ElevatorConstants.Stage1.MASS.in(Kilogram),
+                    Constants.ElevatorConstants.Stage1.DRUM_RADIUS.in(Meter),
+					0,
+                    this.stage1MaxHeight,
+					true,
+					0,
+					0.02,
+					0);
+
+            stage2ElevatorSim = new ElevatorSim(
+					stage2Gearbox,
+					Constants.ElevatorConstants.Stage2.GEAR_RATIO,
+                    Constants.ElevatorConstants.Stage2.MASS.in(Kilogram),
+                    Constants.ElevatorConstants.Stage2.DRUM_RADIUS.in(Meter),
+					0,
+                    this.stage2MaxHeight,
+					true,
+					0,
+					0.02,
+					0);
+        }
+
+		System.out.println("Created ElevatorSubsystem");
+	}
+
+	public Distance getStage1Setpoint() {
+        return Meter.of(stage1Controller.getGoal().position);
+    }
+
+    public Distance getStage2Setpoint() {
+        return Meter.of(stage2Controller.getGoal().position);
+    }
+
+    public void setStage1Setpoint(Distance height) {
+        stage1Controller.setGoal(
+                MathUtil.clamp(height.in(Meter), 0, this.stage1MaxHeight));
+    }
+
+    public void setStage2Setpoint(Distance height) {
+        stage2Controller.setGoal(
+                MathUtil.clamp(height.in(Meter), 0, this.stage2MaxHeight));
+    }
+
+	public Distance getStage1Height() {
+        return Meter.of(stage1Encoder.getPosition());
+    }
+
+    public Distance getStage2Height() {
+        return Meter.of(stage2Encoder.getPosition());
+    }
+
+    public LinearVelocity getStage1HeightVelocity() {
+        return MetersPerSecond.of(stage1Encoder.getVelocity() / 60);
+    }
+
+    public LinearVelocity getStage2HeightVelocity() {
+        return MetersPerSecond.of(stage2Encoder.getVelocity() / 60);
+    }
+
+	/**
+     * Sets the setpoint for stage 1 to the stages current height
+     */
+    public void resetStage1Setpoint() {
+        setStage1Setpoint(getStage1Height());
+    }
+
+    /**
+     * Sets the setpoint for stage 2 to the stages current height
+     */
+    public void resetStage2Setpoint() {
+        setStage2Setpoint(getStage2Height());
+    }
+
+    /**
+     * Gets the offset of the shoulder pivot point from the ground
+     * 
+     * @param includeGroundHeight If the distance from the bot to the ground should be included
+     * @return The pivot points offset from the ground / body pan in meters
+     */
+    public Distance getPivotPointOffset(boolean includeGroundHeight) {
+        return Meter.of(((includeGroundHeight) ? Constants.RobotKinematicConstants.HEIGHT_OFF_GROUND.in(Meter) : 0)
+                + Constants.ElevatorConstants.ZERO_HEIGHTS_ABOVE_BASE.in(Meter)
+                + Constants.ArmConstants.Shoulder.STAGE_OFFSET_UP.in(Meter));
+    }
+
+	/**
+     * Gets the height of both elevator stages relative to the carpet
+     * 
+     * @return The height of the elevator relative to the carpet in meters
+     */
+    public Distance getCarpetElevatorHeight() {
+        return getStage1Height().plus(getStage2Height()).plus(getPivotPointOffset(true));
+    }
+
+    /**
+     * Checks if a target height is reachable by the elevator
+     * 
+     * @param height The target height from the ground in meters
+     * @return Whether the target height is posible
+     */
+    public boolean checkGlobalHeightPossible(Distance height) {
+
+        // Because the elevator even at its lowest is a little bit off the ground its base height has to be subtracted from the overall height to get the local height
+        double elevatorLocalHeight = (height.minus(getPivotPointOffset(true))).in(Meter);
+
+        return elevatorLocalHeight >= 0 && elevatorLocalHeight <= this.maxHeight;
+    }
+
+    /**
+     * Sets the overall height of the elevator by moving both stages
+     * 
+     * @param targetHeight The target height of the elevator relative to the ground in meters
+     */
+    public void setOverallHeight(Distance targetHeight) {
+
+        // Because the target height is based on the distance from the carpet it has to be converted to elevator height
+        double targetHeightMeters = targetHeight.in(Meter) - getPivotPointOffset(true).in(Meter);
+
+        // Clamp the target height so it is within the possible range of the elevator
+        targetHeightMeters = MathUtil.clamp(targetHeightMeters, 0, this.maxHeight);
+
+        // Get the percent of the total height the elevator is going to
+        double targetRatio = targetHeightMeters / maxHeight;
+
+        double target1Height = MathUtil.clamp(
+                this.stage1MaxHeight * targetRatio, 0,
+                this.stage1MaxHeight);
+        double target2Height = MathUtil.clamp(
+                this.stage2MaxHeight * targetRatio, 0,
+                this.stage2MaxHeight);
+
+        // Set each elevators setpoint to the calculated heights
+        setStage1Setpoint(Meter.of(target1Height));
+        setStage2Setpoint(Meter.of(target2Height));
+    }
+
+	@Override
+    public void simulationPeriodic() {
+        stage1ElevatorSim.setInput(stage1MotorSim.getAppliedOutput() * RoboRioSim.getVInVoltage());
+        stage2ElevatorSim.setInput(stage2MotorSim.getAppliedOutput() * RoboRioSim.getVInVoltage());
+
+        // Update every 20 miliseconds
+        stage1ElevatorSim.update(0.02);
+        stage2ElevatorSim.update(0.02);
+
+        // Iterate the simulation of both motors
+        stage1MotorSim.iterate(stage1ElevatorSim.getVelocityMetersPerSecond(),
+                RoboRioSim.getVInVoltage(), 0.02);
+
+        stage2MotorSim.iterate(stage2ElevatorSim.getVelocityMetersPerSecond(),
+                RoboRioSim.getVInVoltage(), 0.02);
+
+        RoboRioSim.setVInVoltage(BatterySim.calculateDefaultBatteryLoadedVoltage(
+                stage1ElevatorSim.getCurrentDrawAmps() + stage2ElevatorSim.getCurrentDrawAmps()));
+
+    }
+
+    @Override
+    public void periodic() {
+
+        double stage1VoltsOutput = MathUtil.clamp(
+                stage1Controller.calculate(getStage1Height().in(Meter)) + stage1FeedFoward
+                        .calculateWithVelocities(getStage1HeightVelocity().in(MetersPerSecond), stage1Controller.getGoal().velocity),
+                -10, 10);
+        stage1Motor.setVoltage(stage1VoltsOutput);
+
+        double stage2VoltsOutput = MathUtil.clamp(
+                stage2Controller.calculate(getStage2Height().in(Meter)) + stage2FeedFoward
+                        .calculateWithVelocities(getStage2HeightVelocity().in(MetersPerSecond), stage2Controller.getGoal().velocity),
+                -10, 10);
+        stage2Motor.setVoltage(stage2VoltsOutput);
+
+    }
 }
